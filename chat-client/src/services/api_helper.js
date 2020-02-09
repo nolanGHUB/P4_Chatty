e2452ab830a7603104158e9072bf54c34a3dbf13
nolanGHUB <nolanrgiles@gmail.com>
2020-02-08 23:08:41 -0500

    [api_helper.js] adjusted login call to return what is needed for the app to function, so on top of the key the name and id

--- chat-client/src/services/api_helper.js
+++ chat-client/src/services/api_helper.js
@@ -9,24 +9,31 @@ export const loginUser = async (loginData) => {
   const resp = await api.post('/auth/login', loginData);
   api.defaults.headers.common.authorization = `Bearer ${resp.data.auth_token}`;
   localStorage.setItem('authToken', resp.data.auth_token);
-  localStorage.setItem('name', resp.data.user.name);
-  localStorage.setItem('email', resp.data.user.email);
-  return resp.data.user;
+  localStorage.setItem('name', resp.data.name);
+  localStorage.setItem('email', resp.data.email);
+  return {
+    auth_token: resp.data.auth_token,
+    name: resp.data.name,
+    email: resp.data.email,
+    id: resp.data.id
+  };
 }
 
 //REGISTER
 export const registerUser = async (registerData) => {
+  let email;
   try {
     const resp = await api.post('/signup', registerData);
     api.defaults.headers.common.authorization = `Bearer ${resp.data.auth_token}`;
     localStorage.setItem('authToken', resp.data.auth_token);
     localStorage.setItem('name', resp.data.user.name);
     localStorage.setItem('email', resp.data.user.email);
+    email = resp.data.user.email;
     return resp.data.user;
   } catch (e) {
     console.log(e.response);
     if (e.response.status === 422) {
-      return { errorMessage: "Email is already associated with a user, please login to" }
+      return { errorMessage: `Email address already in use. You indicated you are a new customer, but an account already exists with the e-mail ${email}` };
     }
   }
 
