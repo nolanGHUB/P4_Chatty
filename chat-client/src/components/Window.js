b6eef19a68fcca8d21c9d906e4136fb7517d15f1
nolanGHUB <nolanrgiles@gmail.com>
2020-02-10 13:48:52 -0500

    [window] built out component as a wrapper component to contain the top windows bar for any other component

--- chat-client/src/components/Window.js
+++ chat-client/src/components/Window.js
@@ -3,7 +3,18 @@ import React from 'react'
 export default function Window(props) {
   return (
     <div>
-
+      <div className="window">
+        <div className="modal-inner">
+          <div
+            className="modal-button"
+            onClick={e => { props.onClose(e) }}>{props.topBarText}
+            <div className='modal-button-box'>X</div>
+          </div>
+          <div className="modal-child">
+            {props.children}
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
