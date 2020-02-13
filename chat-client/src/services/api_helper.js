ac82f02aab79454b89314cf4069112e5dbdd918a
nolanGHUB <nolanrgiles@gmail.com>
2020-02-13 10:45:57 -0500

    [api_helper] added function to grab 10 most recent chat messages

--- chat-client/src/services/api_helper.js
+++ chat-client/src/services/api_helper.js
@@ -6,18 +6,25 @@ const api = axios.create({
 
 //LOGIN
 export const loginUser = async (loginData) => {
-  const resp = await api.post('/auth/login', loginData);
-  api.defaults.headers.common.authorization = `Bearer ${resp.data.auth_token}`;
-  localStorage.setItem('authToken', resp.data.auth_token);
-  localStorage.setItem('name', resp.data.name);
-  localStorage.setItem('email', resp.data.email);
-  localStorage.setItem('id', resp.data.id)
-  return {
-    auth_token: resp.data.auth_token,
-    name: resp.data.name,
-    email: resp.data.email,
-    id: resp.data.id
-  };
+  try {
+    const resp = await api.post('/auth/login', loginData);
+    api.defaults.headers.common.authorization = `Bearer ${resp.data.auth_token}`;
+    localStorage.setItem('authToken', resp.data.auth_token);
+    localStorage.setItem('name', resp.data.name);
+    localStorage.setItem('email', resp.data.email);
+    localStorage.setItem('id', resp.data.id)
+    return {
+      auth_token: resp.data.auth_token,
+      name: resp.data.name,
+      email: resp.data.email,
+      id: resp.data.id
+    };
+  } catch (e) {
+    console.log(e.response);
+    if (e.response && e.response.status === 401) {
+      return { errorMessage: `Email or password is incorrect.` };
+    }
+  }
 }
 
 //REGISTER
@@ -25,16 +32,19 @@ export const registerUser = async (registerData) => {
   let email;
   try {
     const resp = await api.post('/signup', registerData);
+    console.log(resp)
     api.defaults.headers.common.authorization = `Bearer ${resp.data.auth_token}`;
     localStorage.setItem('authToken', resp.data.auth_token);
-    localStorage.setItem('name', resp.data.user.name);
-    localStorage.setItem('email', resp.data.user.email);
-    localStorage.setItem('id', resp.data.user.id)
-    email = resp.data.user.email;
-    return resp.data.user;
+    localStorage.setItem('name', resp.data.name);
+    localStorage.setItem('id', resp.data.id)
+    return {
+      auth_token: resp.data.auth_token,
+      name: resp.data.name,
+      id: resp.data.id
+    };
   } catch (e) {
     console.log(e.response);
-    if (e.response.status === 422) {
+    if (e.response && e.response.status === 422) {
       return { errorMessage: `Email address already in use. You indicated you are a new customer, but an account already exists with the e-mail ${email}` };
     }
   }
@@ -46,5 +56,36 @@ export const verifyUser = async () => {
   const token = localStorage.getItem('authToken');
   if (token) {
     api.defaults.headers.common.authorization = `Bearer ${token}`;
+  }
+}
+
+//GET USERS
+export const getAllUsers = async () => {
+  try {
+    const users = await api.get('/users')
+    return users.data;
+  } catch (e) {
+    console.error(e.message);
+  }
+}
+
+//GET ONLINE USERS
+export const getOnlineUsers = async () => {
+  try {
+    let users = await api.get('/users')
+    users = users.data.filter(user => user.is_online === true);
+    return users;
+  } catch (e) {
+    console.error(e.message);
+  }
+}
+
+//GET LAST 10 MESSAGES
+export const getTenMessages = async () => {
+  try {
+    let messages = await api.get('/chat_messages')
+    return messages.data;
+  } catch (e) {
+    console.error(e.message)
   }
 }
