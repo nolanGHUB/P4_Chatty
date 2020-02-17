5b1f673ab478a816d419645da99d8a98d4def847
nolanGHUB <nolanrgiles@gmail.com>
2020-02-17 00:01:39 -0500

    [index.js] importing new fonts

--- chat-client/src/index.js
+++ chat-client/src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter as Router } from 'react-router-dom'
+import './fonts/px_sans_nouveaux.ttf';
+import './fonts/FuturaStd-Medium.otf';
+import './fonts/ms-sans-serif.ttf';
 
 ReactDOM.render(
   <Router>
