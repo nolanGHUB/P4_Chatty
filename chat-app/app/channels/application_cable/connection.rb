ace5ea95ad7beda44656a5587356be1bec868457
nolanGHUB <nolanrgiles@gmail.com>
2020-02-16 23:55:49 -0500

    [connection.rb] removed old code that no longer served a function

--- chat-app/app/channels/application_cable/connection.rb
+++ chat-app/app/channels/application_cable/connection.rb
@@ -1,4 +1,17 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    #identified_by :current_user
+    #
+    #def connect
+    #  self.current_user = User.find_by(id: cookies.signed[:user_id])
+    #end
+    #
+    #protected
+    #
+    #def find_verified_user
+    #  if (verified_user = User.find_by(id: cookies.signed[:user_id]))
+    #    verified_user
+    #  end
+    #end
   end
 end
