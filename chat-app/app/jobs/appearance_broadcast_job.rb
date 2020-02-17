6c83f24379106b22f89041a42fcfa8880b112aa9
nolanGHUB <nolanrgiles@gmail.com>
2020-02-16 23:56:35 -0500

    [appearance_broadcast_job.rb] Now fully functional to handle appearance and disappearance broadcasts.

--- chat-app/app/jobs/appearance_broadcast_job.rb
+++ chat-app/app/jobs/appearance_broadcast_job.rb
@@ -2,14 +2,25 @@
 class AppearanceBroadcastJob < ApplicationJob
   queue_as :default
 
-  #def perform(user)
-  #  ActionCable.server.broadcast "appearance_#{user.is_online}", render_json(user)
-  #end
-  #
-  #private
-  #
-  #def render_json(user)
-  #  ApplicationController.renderer.render(json: user)
-  #end
+  def perform(user)
+    #ActionCable.server.broadcast "appearance_#{user.is_online}", render_json(user)
+    #ActionCable.server.broadcast "chat_channel", {event: 'appear', id: self.id, name: self.name}
+    #ActionCable.server.broadcast "chat_channel", json_response(user)
+
+    if user.is_online == true
+      stance = 'appear'
+    elsif user.is_online == false
+      stance = 'disappear'
+    end
+    ActionCable.server.broadcast "chat_channel", {event: stance, id: user.id, name: user.name}
+  end
+
+  private
+
+  def render_json(user)
+    #ApplicationController.renderer.render(json: user)
+    clean_user = {"id" => user.id, "name" => user.name, "email" => user.email, "is_online" => user.is_online}
+    json_response(user)
+  end
 
 end
