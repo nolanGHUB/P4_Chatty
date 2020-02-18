fbd24f5cf07a9c9ff39b5d57fa765836f59e4716
nolanGHUB <nolanrgiles@gmail.com>
2020-02-18 14:59:20 -0500

    [chat_messages_controller.rb] adjusted the query for most recent messages, now even more complicated
    :

--- chat-app/app/controllers/chat_messages_controller.rb
+++ chat-app/app/controllers/chat_messages_controller.rb
@@ -2,7 +2,7 @@ class ChatMessagesController < ApplicationController
   skip_before_action :authorize_request, only: :create
 
   def index
-    @messages = User.joins(:chat_messages).select("chat_messages.id", "users.name", "chat_messages.content", "chat_messages.created_at").order("chat_messages.id").last(10)
+    @messages = User.joins(:chat_messages).select("chat_messages.id", "users.name", "chat_messages.content", "chat_messages.created_at", "chat_messages.destination").where("chat_messages.destination = 0").order("chat_messages.id").last(10)
     json_response(@messages)
   end
 end
