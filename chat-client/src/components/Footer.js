9d7d9b291b362835caaff99ab50c5d388fcfc644
nolanGHUB <nolanrgiles@gmail.com>
2020-02-19 12:05:18 -0500

    [footer] Created simple footer

---
+++ chat-client/src/components/Footer.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+export default function Footer() {
+
+  return (
+    <div className="footer">
+      <div className="footer-left">
+        <div>
+          A web-based remaking of the now "retro" chat-app "Aol Instant Messenger"
+        </div>
+
+      </div>
+      <div className="footer-right">
+        <div>
+          Nolan Giles - GA - 2020
+        </div>
+        <div className="footer-links">
+          <a href="https://www.linkedin.com/in/nolangiles/">LinkedIn</a> -
+          <a href="https://github.com/nolanGHUB/P4_Chatty">Github</a>
+        </div>
+
+      </div>
+    </div>
+  )
+}
